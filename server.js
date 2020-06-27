ea0270a3ae4e9721cf2dd7123d47b852478ed6ec
Rudi <rudix.lab@gmail.com>
2020-06-27 18:45:17 +0300

    c

--- server.js
+++ server.js
@@ -1,40 +1,15 @@
 require('dotenv').config()
 const serverless = require('serverless-http')
 const express = require('express')
-const bodyParser = require('body-parser')
-const isBot = require('isbot-fast')
-const db = require('./src/db.js')
+
+//const db = require('./src/db.js')
 const app = express()
-app.use(bodyParser.json())
+//app.use(bodyParser.json())
+const PouchDB = require('pouchdb')
+var TempPouchDB = PouchDB.defaults({ prefix: '/tmp/' })
 
-app.get('/', function (req, res) {
-    const ua = req.headers['user-agent'] || ''
+app.use('/', require('express-pouchdb')(TempPouchDB))
 
-    if (isBot(ua)) {
-        res.end(ua)
-    } else {
-        res.end('not bot')
-        // Making cookies
-    }
-})
-
-db.put({ id: 'test' }, function (data) {
-    console.log(data)
-})
-db.get('test', function (data) {
-    console.log(data)
-})
-
-app.get('/:id', function (req, res) {
-    db.get(req.params.id, function (data) {
-        res.json(data)
-    })
-})
-app.post('/', function (req, res) {
-    db.put(req.body, function (data) {
-        res.json(data)
-    })
-})
 if (!process.env.LAMBDA_RUNTIME_DIR) {
     app.listen(3000)
 }
