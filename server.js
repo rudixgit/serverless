e8ffd42865edfd2a78636ecc8131baf406cf8ec0
Rudi <rudix.lab@gmail.com>
2020-06-29 12:21:32 +0300

    c

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.post('/ddb/', async (req, res) => {
 
 app.get('/:appid/:id', async (req, res) => {
     const data = await db.get(req.params.id)
-    let template = ejs.render('str', {})
+    let template = ejs.render(fs.readFileSync('./views/rudix.html', 'utf8'), {})
     res.end(template)
     //console.log(data)
 
