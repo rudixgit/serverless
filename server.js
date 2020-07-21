dc26d2c62fb2cc88ad46cb91365850e1d4b80184
Radoslav <arpecop@mac.local>
2020-07-21 12:36:14 +0300

    ddd

--- server.js
+++ server.js
@@ -1,204 +1,162 @@
-require('dotenv').config()
-const serverless = require('serverless-http')
-const express = require('express')
-const bodyParser = require('body-parser')
-const isBot = require('isbot-fast')
-const ejs = require('ejs')
-const app = express()
-const compression = require('compression')
-const jwt = require('jsonwebtoken')
-const accessTokenSecret = 'youraccesstokensecret'
-const users = [
-    {
-        username: '1',
-        password: '1',
-        role: 'admin',
-    },
-    {
-        username: 'anna',
-        password: 'password123member',
-        role: 'member',
-    },
-]
-app.use(compression())
-app.use(bodyParser.json())
-app.use(express.static('public'))
+require("dotenv").config();
+const serverless = require("serverless-http");
+const express = require("express");
+const bodyParser = require("body-parser");
+const isBot = require("isbot-fast");
+const ejs = require("ejs");
+const app = express();
+const compression = require("compression");
+const jwt = require("jsonwebtoken");
+const accessTokenSecret = "youraccesstokensecret";
 
-const { get, put, query } = require('./src/db.js')
-const { getS3 } = require('./src/s3.js')
-const { timeline, readFile, writeFile } = require('./src/twitter.js')
+app.use(compression());
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+const { get, put, query } = require("./src/db.js");
+const { getS3 } = require("./src/s3.js");
+const { timeline, readFile, writeFile } = require("./src/twitter.js");
 
 app.use(async function (req, res, next) {
-    const json = {
-        path: 'https://rudixlab.com' + req.path,
-        t: new Date(),
-        ua: req.headers['user-agent'],
-    }
-    const prev = await readFile('/tmp/log.txt')
-    await writeFile(
-        '/tmp/log.txt',
-        prev
-            ? '\n' + JSON.stringify(json) + ',' + prev + ''
-            : JSON.stringify(json)
-    )
-    next()
-})
-app.get('/', async (req, res) => {
-    res.header('Content-Type', 'text/html')
+  const json = {
+    path: "https://rudixlab.com" + req.path,
+    t: new Date(),
+    ua: req.headers["user-agent"],
+  };
+  const prev = await readFile("/tmp/log.txt");
+  await writeFile(
+    "/tmp/log.txt",
+    prev ? "\n" + JSON.stringify(json) + "," + prev + "" : JSON.stringify(json)
+  );
+  next();
+});
+app.get("/", async (req, res) => {
+  res.header("Content-Type", "text/html");
 
-    const contents = await getS3('views/rudix.html')
-    const json = await readFile('./views/rudix.json')
+  const contents = await getS3("views/rudix.html");
+  const json = await readFile("./views/rudix.json");
 
-    res.end(ejs.render(contents.Body.toString(), JSON.parse(json)))
-})
-app.get('/robots.txt', (req, res) => {
-    res.sendFile(__dirname + '/views/robots.txt')
-})
-app.get('/date', (req, res) => {
-    res.end(new Date().toString())
-})
+  res.end(ejs.render(contents.Body.toString(), JSON.parse(json)));
+});
+app.get("/robots.txt", (req, res) => {
+  res.sendFile(__dirname + "/views/robots.txt");
+});
 
-app.get('/ddb/:id', async (req, res) => {
-    const data = await get(req.params.id)
-    res.json(data)
-})
-app.post('/ddb/', async (req, res) => {
-    const data = await put(req.body)
-    res.json(data)
-})
-app.get('/log', async (req, res) => {
-    const contents = await readFile('/tmp/log.txt')
-    res.end('[' + contents + ']')
-})
-app.get('/sitemap/', async function (req, res) {
-    res.header('Content-Type', 'text/plain')
-    const data = await query({
-        id: Math.round(req.query.id || 1),
-        collection: 't',
-        limit: 50000,
-        descending: true,
-    })
+app.get("/ddb/:id", async (req, res) => {
+  const data = await get(req.params.id);
+  res.json(data);
+});
+app.post("/ddb/", async (req, res) => {
+  const data = await put(req.body);
+  res.json(data);
+});
+app.get("/log", async (req, res) => {
+  const contents = await readFile("/tmp/log.txt");
+  res.end("[" + contents + "]");
+});
+app.get("/sitemap/", async function (req, res) {
+  res.header("Content-Type", "text/plain");
+  const data = await query({
+    id: Math.round(req.query.id || 1),
+    collection: "t",
+    limit: 50000,
+    descending: true,
+  });
 
-    res.end(
-        data.Items.map(
-            (item) =>
-                'https://rudixlab.com/t/' + item.vreme + '/' + item.u + '/'
-        ).join('\n')
-    )
-})
+  res.end(
+    data.Items.map(
+      (item) => "https://rudixlab.com/t/" + item.vreme + "/" + item.u + "/"
+    ).join("\n")
+  );
+});
 
-app.get('/i/:id', async (req, res) => {
-    const ua = req.headers['user-agent'] || ''
-    if (isBot(ua)) {
-        await downloadFile(req.params.id)
-        res.sendFile('/tmp/test.jpg')
-    } else {
-        res.render('kartinki', { id: req.params.id })
-    }
-})
+app.get("/i/:id", async (req, res) => {
+  const ua = req.headers["user-agent"] || "";
+  if (isBot(ua)) {
+    await downloadFile(req.params.id);
+    res.sendFile("/tmp/test.jpg");
+  } else {
+    res.render("kartinki", { id: req.params.id });
+  }
+});
 
-app.get('/s3/*', async (req, res) => {
-    const contents = await getS3(req.path.replace('/s3/', ''))
-    res.header('Content-Type', contents.ContentType)
-    res.end(contents.Body)
-})
+app.get("/s3/*", async (req, res) => {
+  const contents = await getS3(req.path.replace("/s3/", ""));
+  res.header("Content-Type", contents.ContentType);
+  res.end(contents.Body);
+});
 
-app.get('/t/:time/:id', async (req, res) => {
-    res.header('Content-Type', 'text/html')
-    const { time, id } = req.params
+app.get("/t/:time/:id", async (req, res) => {
+  res.header("Content-Type", "text/html");
+  const { time, id } = req.params;
 
-    const data = await query({
-        id: Math.round(time),
-        collection: 't',
-        limit: 10,
-        descending: true,
-    })
-    const tweets = await timeline(id)
+  const data = await query({
+    id: Math.round(time),
+    collection: "t",
+    limit: 10,
+    descending: true,
+  });
+  const tweets = await timeline(id);
 
-    const user = tweets[0]
-        ? tweets[0].user
-        : {
-              profile_image_url_https: 'http://twivatar.glitch.me/' + id,
-              profile_background_color: 'black',
-          }
+  const user = tweets[0]
+    ? tweets[0].user
+    : {
+        profile_image_url_https: "http://twivatar.glitch.me/" + id,
+        profile_background_color: "black",
+      };
 
-    const tags = tweets[0]
-        ? tweets
-              .map((item) => item.text)
-              .join(' ')
-              .split(' ')
-              .filter(function (n) {
-                  if (/#/.test(n)) return n.replace('#', '')
-              })
-        : []
-
-    const contents = await getS3('views/t.html')
-    res.end(
-        ejs.render(contents.Body.toString(), {
-            ...data,
-            tweets,
-            user,
-            tags,
-            //tweets_stringified: JSON.stringify(tweets, null, 4),
-            ...req.params,
+  const tags = tweets[0]
+    ? tweets
+        .map((item) => item.text)
+        .join(" ")
+        .split(" ")
+        .filter(function (n) {
+          if (/#/.test(n)) return n.replace("#", "");
         })
-    )
-})
-app.get('/:colid/:time/:id', async (req, res) => {
-    res.header('Content-Type', 'text/html')
-    const { time, id, colid } = req.params
+    : [];
 
-    const data = await query({
-        id: Math.round(time),
-        collection: colid,
-        limit: 10,
-        descending: true,
+  const contents = await getS3("views/t.html");
+  res.end(
+    ejs.render(contents.Body.toString(), {
+      ...data,
+      tweets,
+      user,
+      tags,
+      //tweets_stringified: JSON.stringify(tweets, null, 4),
+      ...req.params,
     })
-    const contents = await getS3('views/' + colid + '.html')
+  );
+});
+app.get("/:colid/:time/:id", async (req, res) => {
+  res.header("Content-Type", "text/html");
+  const { time, id, colid } = req.params;
 
-    res.end(
-        ejs.render(contents.Body.toString(), {
-            ...data,
-            ...req.params,
-        })
-    )
-})
-app.get('/:appid/:id', async (req, res) => {
-    res.header('Content-Type', 'text/html')
-    const { appid, id } = req.params
-    const data = await get(id)
-    const contents = await getS3('views/' + appid + '.html')
-    res.end(ejs.render(contents.Body.toString(), { ...data, ...req.query }))
-})
+  const data = await query({
+    id: Math.round(time),
+    collection: colid,
+    limit: 10,
+    descending: true,
+  });
+  const contents = await getS3("views/" + colid + ".html");
 
-app.post('/login', (req, res) => {
-    // read username and password from request body
-    const { username, password } = req.body
-    console.log(req.body)
-
-    // filter user from the users array by username and password
-    const user = users.find((u) => {
-        return u.username === username && u.password === password
+  res.end(
+    ejs.render(contents.Body.toString(), {
+      ...data,
+      ...req.params,
     })
-
-    if (user) {
-        // generate an access token
-        const accessToken = jwt.sign(
-            { username: user.username, role: user.role },
-            accessTokenSecret,
-            { expiresIn: '20m' }
-        )
-
-        res.json({
-            accessToken,
-        })
-    } else {
-        res.json({})
-    }
-})
+  );
+});
+app.get("/:appid/:id", async (req, res) => {
+  res.header("Content-Type", "text/html");
+  const { appid, id } = req.params;
+  const data = await get(id);
+  const contents = await getS3("views/" + appid + ".html");
+  res.end(ejs.render(contents.Body.toString(), { ...data, ...req.query }));
+});
 
 if (!process.env.LAMBDA_RUNTIME_DIR) {
-    app.listen(process.env.PORT || 3000)
+  app.listen(process.env.PORT || 3000);
+  console.log("working on localhost");
 }
 //dsdsdsdsaddsadsadasdas
-module.exports.handler = serverless(app)
+module.exports.handler = serverless(app);
