44bf809e17fad751e4d7d23a93caf17459cead1f
Rudi <rudix.lab@gmail.com>
2021-08-03 19:34:37 +0300

    dsadsa

--- server.js
+++ server.js
@@ -1,132 +1,131 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const cluster = require('cluster');
- 
-const numCPUs = require('os').cpus().length;
+const cluster = require('cluster')
+
+const numCPUs = require('os').cpus().length
 
 if (cluster.isPrimary) {
-  console.log(`Primary ${process.pid} is running`);
-
   // Fork workers.
   for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
+    cluster.fork()
   }
 
   cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-  });
+    console.log(`worker ${worker.process.pid} died`)
+    cluster.fork()
+  })
 } else {
-  const serverless = require("serverless-http");
-  const express = require("express");
-  const bodyParser = require("body-parser");
-  const ejs = require("ejs");
+  const serverless = require('serverless-http')
+  const express = require('express')
+  const bodyParser = require('body-parser')
+  const ejs = require('ejs')
 
-  const app = express();
+  const app = express()
 
-  const compression = require("compression");
-  const cors = require("cors");
+  const compression = require('compression')
+  const cors = require('cors')
 
-  app.use(compression());
-  app.use(cors());
-  app.use(bodyParser.json());
+  app.use(compression())
+  app.use(cors())
+  app.use(bodyParser.json())
 
-  const { put, query, q1 } = require("./src/db.js");
-  const { getS3, getFS } = require("./src/s3.js");
-  const { timeline, readFile, writeFile } = require("./src/twitter.js");
+  const { put, query, q1 } = require('./src/db.js')
+  const { getS3, getFS } = require('./src/s3.js')
+  const { timeline, readFile, writeFile } = require('./src/twitter.js')
 
   app.use(async (req, res, next) => {
     const json = {
       path: `https://rudixlab.com${req.path}`,
       t: new Date(),
-      ua: req.headers["user-agent"],
-    };
-    const prev = await readFile("/tmp/log.txt");
+      ua: req.headers['user-agent']
+    }
+    const prev = await readFile('/tmp/log.txt')
     await writeFile(
-      "/tmp/log.txt",
+      '/tmp/log.txt',
       prev ? `\n${JSON.stringify(json)},${prev}` : JSON.stringify(json)
-    );
-    next();
-  });
-  app.get("/", async (req, res) => {
-    res.header("Content-Type", "text/html");
+    )
+    next()
+  })
+  app.get('/', async (req, res) => {
+    res.header('Content-Type', 'text/html')
 
-    const contents = await getFS("./views/rudix.html");
-    const json = await readFile("./views/rudix.json");
+    const contents = await getFS('./views/rudix.html')
+    const json = await readFile('./views/rudix.json')
 
-    res.end(ejs.render(contents.Body.toString(), JSON.parse(json)));
-  });
-  app.get("/robots.txt", (req, res) => {
-    res.sendFile(`${__dirname}/views/robots.txt`);
-  });
-  app.post("/db/q1", async (req, res) => {
-    const data = await q1(req.body);
-    res.json(data);
-  });
+    res.end(ejs.render(contents.Body.toString(), JSON.parse(json)))
+  })
+  app.get('/robots.txt', (req, res) => {
+    res.sendFile(`${__dirname}/views/robots.txt`)
+  })
+  app.post('/db/q1', async (req, res) => {
+    const data = await q1(req.body)
+    res.json(data)
+  })
 
-  app.post("/db/", async (req, res) => {
-    const data = await query(req.body);
-    res.json(data);
-  });
-  app.post("/dbput/", async (req, res) => {
-    const data = await put(req.body);
-    res.json(data);
-  });
-  app.get("/log", async (req, res) => {
-    const contents = await readFile("/tmp/log.txt");
-    res.end(`[${contents}]`);
-  });
-  app.get("/sitemap/", async (req, res) => {
-    res.header("Content-Type", "text/plain");
+  app.post('/db/', async (req, res) => {
+    const data = await query(req.body)
+    res.json(data)
+  })
+  app.post('/dbput/', async (req, res) => {
+    const data = await put(req.body)
+    res.json(data)
+  })
+  app.get('/log', async (req, res) => {
+    const contents = await readFile('/tmp/log.txt')
+    res.end(`[${contents}]`)
+  })
+  app.get('/sitemap/', async (req, res) => {
+    res.header('Content-Type', 'text/plain')
     const data = await query({
       id: Math.round(req.query.id || 1),
-      collection: "t",
+      collection: 't',
       limit: 50000,
-      descending: true,
-    });
+      descending: true
+    })
 
     res.end(
       data.Items.map(
-        (item) => `https://rudixlab.com/t/${item.vreme}/${item.u}/`
-      ).join("\n")
-    );
-  });
+        item => `https://rudixlab.com/t/${item.vreme}/${item.u}/`
+      ).join('\n')
+    )
+  })
 
-  app.get("/s3/*", async (req, res) => {
-    const contents = await getS3(req.path.replace("/s3/", ""));
-    res.header("Content-Type", contents.ContentType);
-    res.end(contents.Body);
-  });
+  app.get('/s3/*', async (req, res) => {
+    const contents = await getS3(req.path.replace('/s3/', ''))
+    res.header('Content-Type', contents.ContentType)
+    res.end(contents.Body)
+  })
 
-  app.get("/t/:time/:id", async (req, res) => {
-    const { time, id } = req.params;
+  app.get('/t/:time/:id', async (req, res) => {
+    const { time, id } = req.params
 
     const data = await query({
       id: Math.round(time),
-      collection: "t",
+      collection: 't',
       limit: 10,
-      descending: true,
-    });
+      descending: true
+    })
 
-    const tweets = await timeline(id);
+    const tweets = await timeline(id)
 
     const user = tweets[0]
       ? tweets[0].user
       : {
-        profile_image_url_https: `http://twivatar.glitch.me/${id}`,
-        profile_background_color: "black",
-      };
+          profile_image_url_https: `http://twivatar.glitch.me/${id}`,
+          profile_background_color: 'black'
+        }
 
     const tags = tweets[0]
       ? tweets
-        .map((item) => item.text)
-        .join(" ")
-        .split(" ")
-        .filter(function (n) {
-          if (/#/.test(n)) return n.replace("#", "");
-        })
-      : [];
+          .map(item => item.text)
+          .join(' ')
+          .split(' ')
+          .filter(function (n) {
+            if (/#/.test(n)) return n.replace('#', '')
+          })
+      : []
 
-    const contents = await getFS("views/t.html");
+    const contents = await getFS('views/t.html')
 
     const jsonOutput = {
       Items: [],
@@ -135,53 +134,53 @@ if (cluster.isPrimary) {
       user,
       tags,
       // tweets_stringified: JSON.stringify(tweets, null, 4),
-      ...req.params,
-    };
-    if (req.query.format) {
-      res.header("Content-Type", "application/json");
-      res.json(jsonOutput);
-    } else {
-      res.header("Content-Type", "text/html");
-      res.end(ejs.render(contents.Body.toString(), jsonOutput));
+      ...req.params
     }
-  });
-  app.get("/:colid/:time/:id", async (req, res) => {
-    res.header("Content-Type", "text/html");
-    const { time, colid } = req.params;
+    if (req.query.format) {
+      res.header('Content-Type', 'application/json')
+      res.json(jsonOutput)
+    } else {
+      res.header('Content-Type', 'text/html')
+      res.end(ejs.render(contents.Body.toString(), jsonOutput))
+    }
+  })
+  app.get('/:colid/:time/:id', async (req, res) => {
+    res.header('Content-Type', 'text/html')
+    const { time, colid } = req.params
 
     const data = await query({
       id: Math.round(time),
       collection: colid,
       limit: 10,
-      descending: true,
-    });
-    const contents = await getFS(`views/${colid}.html`);
+      descending: true
+    })
+    const contents = await getFS(`views/${colid}.html`)
 
     res.end(
       ejs.render(contents.Body.toString(), {
         ...data,
-        ...req.params,
+        ...req.params
       })
-    );
-  });
-  app.get("/:appid/:id", async (req, res) => {
-    res.header("Content-Type", "text/html");
-    const { appid, id } = req.params;
+    )
+  })
+  app.get('/:appid/:id', async (req, res) => {
+    res.header('Content-Type', 'text/html')
+    const { appid, id } = req.params
 
     const data = await query({
       id: Math.round(id),
       collection: appid,
       limit: 10,
-      descending: true,
-    });
+      descending: true
+    })
 
-    const contents = await getFS(`views/${appid}.html`);
-    res.end(ejs.render(contents.Body.toString(), { ...data, ...req.query }));
-  });
+    const contents = await getFS(`views/${appid}.html`)
+    res.end(ejs.render(contents.Body.toString(), { ...data, ...req.query }))
+  })
 
   if (!process.env.LAMBDA_RUNTIME_DIR) {
-    app.listen(process.env.PORT || 3000);
+    app.listen(process.env.PORT || 3000)
   }
   // dsdsdsdsaddsadsadasdas
-  module.exports.handler = serverless(app);
+  module.exports.handler = serverless(app)
 }
--- src/db.js
+++ src/db.js
@@ -1,109 +1,109 @@
-const AWS = require("aws-sdk");
-const fs = require("fs");
+const AWS = require('aws-sdk')
+const fs = require('fs')
 // Set the region
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   sessionToken: process.env.AWS_SESSION_TOKEN,
-  region: "eu-central-1",
-});
-const db = new AWS.DynamoDB.DocumentClient();
+  region: 'eu-central-1'
+})
+const db = new AWS.DynamoDB.DocumentClient()
 
-async function readFile(path) {
-  return new Promise((resolve) => {
-    fs.readFile(`/tmp/${path}`, "utf8", (err, data) => {
+async function readFile (path) {
+  return new Promise(resolve => {
+    fs.readFile(`/tmp/${path}`, 'utf8', (err, data) => {
       if (err) {
-        resolve(null);
+        resolve(null)
       }
-      resolve(data);
-    });
-  });
+      resolve(data)
+    })
+  })
 }
-async function get(id) {
-  const cached = await readFile(id);
+async function get (id) {
+  const cached = await readFile(id)
   const params = {
-    TableName: "ddb",
-    KeyConditionExpression: "tip = :hkey",
+    TableName: 'ddb',
+    KeyConditionExpression: 'tip = :hkey',
     ExpressionAttributeValues: {
-      ":hkey": id,
+      ':hkey': id
     },
-    ScanIndexForward: false,
-  };
-  return new Promise((resolve) => {
+    ScanIndexForward: false
+  }
+  return new Promise(resolve => {
     if (!cached) {
       db.query(params, function (err, data) {
-        const x = data.Count >= 1 ? data.Items[0] : {};
+        const x = data.Count >= 1 ? data.Items[0] : {}
         fs.writeFile(`/tmp/${id}`, JSON.stringify(x), function () {
-          resolve(x);
-        });
-      });
+          resolve(x)
+        })
+      })
     } else {
-      resolve({ ...JSON.parse(cached), cached: true });
+      resolve({ ...JSON.parse(cached), cached: true })
     }
-  });
+  })
 }
 
-function put(json) {
-  return new Promise((resolve) => {
-    db.put({ TableName: "ddb", Item: json }, function () {
-      resolve({});
-    });
-  });
+function put (json) {
+  return new Promise(resolve => {
+    db.put({ TableName: 'ddb', Item: json }, function () {
+      resolve({})
+    })
+  })
 }
 
-async function q1({ fields, collection, descending, limit }) {
+async function q1 ({ fields, collection, descending, limit }) {
   const params = {
-    TableName: "ddb",
-    KeyConditionExpression: "tip = :hkey  and vreme >= :zkey",
+    TableName: 'ddb',
+    KeyConditionExpression: 'tip = :hkey  and vreme >= :zkey',
     FilterExpression: `${Object.keys(query)[0]} = :ukey`,
     ExpressionAttributeValues: {
-      ":zkey": 1,
-      ":hkey": collection,
-      ":ukey": Object.values(query)[0],
+      ':zkey': 1,
+      ':hkey': collection,
+      ':ukey': Object.values(query)[0]
     },
     Limit: limit || 100,
     ScanIndexForward: descending || true,
-    ReturnConsumedCapacity: "TOTAL",
-  };
-  if (fields) {
-    params.ProjectionExpression = fields;
+    ReturnConsumedCapacity: 'TOTAL'
   }
-  return new Promise((resolve) => {
+  if (fields) {
+    params.ProjectionExpression = fields
+  }
+  return new Promise(resolve => {
     db.query(params, (err, data) => {
       if (data.Count === 1) {
-        resolve(data.Items[0]);
+        resolve(data.Items[0])
       }
-      resolve(data);
-    });
-  });
+      resolve(data)
+    })
+  })
 }
 
-async function query({ id, collection, limit, descending, count, fields }) {
+async function query ({ id, collection, limit, descending, count, fields }) {
   const params = {
-    TableName: "ddb",
-    KeyConditionExpression: "tip = :hkey and vreme >= :ukey",
+    TableName: 'ddb',
+    KeyConditionExpression: 'tip = :hkey and vreme >= :ukey',
     ExpressionAttributeValues: {
-      ":hkey": collection,
-      ":ukey": id || 1,
+      ':hkey': collection,
+      ':ukey': id || 1
     },
     Limit: limit || 100,
     ScanIndexForward: descending || false,
-    ReturnConsumedCapacity: "TOTAL",
-  };
+    ReturnConsumedCapacity: 'TOTAL'
+  }
   if (fields) {
-    params.ProjectionExpression = fields;
+    params.ProjectionExpression = fields
   }
   if (count) {
-    params.Select = "COUNT";
+    params.Select = 'COUNT'
   }
-  return new Promise((resolve) => {
+  return new Promise(resolve => {
     db.query(params, (err, data) => {
       if (data.Count === 1) {
-        resolve(data.Items[0]);
+        resolve(data.Items[0])
       }
-      resolve(data);
-    });
-  });
+      resolve(data)
+    })
+  })
 }
 
-module.exports = { get, put, query, q1 };
+module.exports = { get, put, query, q1 }
